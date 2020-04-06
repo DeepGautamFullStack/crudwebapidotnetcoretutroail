fdef141105b1412628f4e11e0582caee09a99080
Deep Gautam <dgautam.hcl@gmail.com>
2020-04-06 15:32:44 +0530

    Crud appi tutorial with swagger by dotnet techy.

---
+++ CrudOperatiosnWebAPI/Data/CrudOperatiosnWebAPIContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperatiosnWebAPI.Models
+{
+    public class CrudOperatiosnWebAPIContext : DbContext
+    {
+        public CrudOperatiosnWebAPIContext (DbContextOptions<CrudOperatiosnWebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CrudOperatiosnWebAPI.Models.User> User { get; set; }
+    }
+}
